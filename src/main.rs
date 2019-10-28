b9d4872bd9dcc5cbab83171ca82d65ba6a1575ab
Keith Marshall <keith.marshall@enginegroup.com>
2019-10-28 16:45:53 +0000

    Take country on command line instead of city

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ mod distance_calculator;
 mod route_finder;
 
 fn main() {
+    // TODO: try to use Rayon for parallelisation (will need some changes to code)
+    let countries = countries_reader::read_countries_from_file("resources/countries.json").unwrap();
+    let city_map = country_list_ops::generate_lookup_map(&countries);
+
     let mut target_city = "Washington D.C.";
     let mut target_range = 2000.0;
 
@@ -15,13 +19,12 @@ fn main() {
         println!("{}", args.join(" "));
         let sections: Vec<&str> = args[1].split('|').collect();
         if sections.len() == 2 {
-            target_city = sections[1];
+            let target_country = sections[1];
+            target_city = &countries.iter().find(|c| c.name == target_country).unwrap().capital_city;
             target_range = sections[0].parse().unwrap();
         }
     }
 
-    let countries = countries_reader::read_countries_from_file("resources/countries.json").unwrap();
-    let city_map = country_list_ops::generate_lookup_map(&countries);
     let rf = route_finder::RouteFinder(&city_map);
     let route = rf.find_route(target_city, target_range, &vec![]);
 
