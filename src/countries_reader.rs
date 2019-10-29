bb6cdb113be51f704cd2a34b44202303ebcfb413
Keith Marshall <keith.marshall@enginegroup.com>
2019-10-29 16:55:00 +0000

    Cleanup

--- src/countries_reader.rs
+++ src/countries_reader.rs
@@ -1,13 +1,9 @@
-use serde::{self, Deserialize}; // , Deserializer};
-// use serde::Deserialize;
-// use serde_aux::prelude::*;
-
+use serde::{self, Deserialize}; 
 
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
 
-// use std::path::Path;
 use crate::country::Country;
 
 #[derive(Deserialize, Debug)]
@@ -19,7 +15,6 @@ struct Countries {
     countries: Vec<Country>
 }
 
-// fn read_countries_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Country>, Box<dyn Error>> {
 pub fn read_countries_from_file(path: &str) -> Result<Vec<Country>, Box<dyn Error>> {
     // Open the file in read-only mode with buffer.
     let file = File::open(path)?;
@@ -43,13 +38,7 @@ mod tests {
     fn test_load_file() {
         let countries = read_countries_from_file("resources/countries.json").unwrap();
 
-        assert_eq!(countries.len(), 211); // 304
+        assert_eq!(countries.len(), 211);
         assert_eq!(countries[0].id, "ABW");
-
-        // assert_eq!(countries[2].id, "AFR");
-        // assert_eq!(countries[2].longitude, "");
-        // assert_eq!(countries[2].latitude, "");
-
-        // println!("{:?}", countries[0]);
     }
 }
