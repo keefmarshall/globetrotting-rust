db208300c80ad6652d31e516cb0a4dff59c88323
Keith Marshall <keith.marshall@enginegroup.com>
2019-10-29 14:06:26 +0000

    Change visited vec into references for better clone performance

--- src/route_finder.rs
+++ src/route_finder.rs
@@ -4,14 +4,15 @@ use crate::country_list_ops::*;
 pub struct RouteFinder<'a>(pub &'a CitiesByDistance);
 
 impl RouteFinder<'_> {
-    pub fn find_route(&self, city: &str, range: f64, visited: &Vec<String>) -> Vec<String> {
+    pub fn find_route(&self, city: &str, range: f64, visited: &Vec<&str>) -> Vec<String> {
         let mut others: Vec<(&str, f64)> = vec![];
         for (other, distance) in &self.0[city] {
             if distance > &range {
                 break;
             }
 
-            if !visited.contains(other) {
+            let other_ref: &str = &other;
+            if !visited.contains(&other_ref) {
                 others.push((other, range - distance));
             }
         }
@@ -19,7 +20,7 @@ impl RouteFinder<'_> {
         let mut route = vec![String::from(city)];
         if others.len() > 0 {
             let mut new_visited = visited.clone();
-            new_visited.push(String::from(city));
+            new_visited.push(city);
 
             let mut longest = others.par_iter()
                 .map( |(o,r)| self.find_route(&o, *r, &new_visited) )
